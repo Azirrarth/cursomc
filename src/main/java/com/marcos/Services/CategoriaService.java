67b4e8a58e62973abd8c07256ee5a92aea5c179b
Marcos Domingues <azirrarth@gmail.com>
2019-04-26 10:45:04 -0300

    Correções em categoriaService e contrutor de pagamento

--- src/main/java/com/marcos/Services/CategoriaService.java
+++ src/main/java/com/marcos/Services/CategoriaService.java
@@ -33,12 +33,12 @@ public class CategoriaService {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	public Categoria update (Categoria obj)
-	{
-		find(obj.getId());
-		return repo.save(obj);
+	public Categoria update(Categoria obj) {
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
-	
+
 	public void delete(Integer id) 
 	{
 		find(id);
@@ -67,4 +67,8 @@ public class CategoriaService {
 	{
 		return new Categoria(objDto.getId(), objDto.getNome());
 	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+		}
 }
